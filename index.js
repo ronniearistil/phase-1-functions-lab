cb496e4eb50ef97f898f36f92e3dd825fd6437a0
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-09-14 10:34:02 -0700

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0; // The first 400 feet are free
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot for feet over 400
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // Flat fare of $25
+    } else {
+      return 'cannot travel that far'; // Over 2500 feet
+    }
+  }
+  
